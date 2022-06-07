434d2fc667acd4eb9986e4d6d6c6825593427c7d
kpotoh <axepon@mail.ru>
2022-06-07 15:50:21 +0200

    corrplot + cca done

--- scripts/cca.r
+++ scripts/cca.r
@@ -1,9 +1,11 @@
+# https://www.rdocumentation.org/packages/vegan/versions/2.4-2/topics/plot.cca
+
 library('vegan')
 
 setwd("valerik/data/")
 
 otus <- read.csv("processed/otu_table_for_cca1.csv")
-metalls <- read.csv("metalls.csv")
+metalls <- read.csv("processed/metalls.csv")
 X <-  otus[2:ncol(otus)]
 Y <- metalls[3:ncol(metalls)]
 rownames(X) <- otus$Samples
@@ -37,10 +39,25 @@ cat(round(sum(mplants_CCA$CCA$eig[1:2])
 # plot(mplants_CCA, scaling = 1, main="Scaling1")
 
 balt.cca <- cca(X, Y)
-plot(balt.cca)
+mod <- balt.cca
+balt.cca$CCA$biplot
+balt.cca$grand.total
+head(summary(balt.cca), tail=5)
+importance <- summary(balt.cca, display=c("wa"))$concont$importance
+xlabel <- paste("CCA1 (", round(importance["Proportion Explained", "CCA1"], 3) * 100, "%)", sep="")
+ylabel <- paste("CCA2 (", round(importance["Proportion Explained", "CCA2"], 3) * 100, "%)", sep="")
+
+# scaling="sites"
+png(file="../figures/CCA.png", width=700, height=500, pointsize = 13)
+plot(balt.cca, type="p", display = c("wa","cn"), ylim=range(-1.6, 1.9), xlab=xlabel, ylab=ylabel,)
+points(balt.cca, "species", pch = 3, cex = .4, col = rgb(red = 1, green = 0, blue = 0, alpha = 0.3))
+points(balt.cca, pch=21, col="red", bg="green", cex=1.2, )
+text(balt.cca, "sites", col="darkgreen", cex=0.9, font=2, pos=3, offset=.7)
+dev.off()
 
 balt.rda <- rda(X, Y)
 plot(balt.rda)
+
 
 
 data(varespec)
@@ -49,7 +66,7 @@ data(varechem)
 ## environmental data matrix
 vare.cca <- cca(varespec, varechem)
 vare.cca
-plot(vare.cca)
+plot(vare.cca, )
 ## Formula interface and a better model
 vare.cca <- cca(varespec ~ Al + P*(K + Baresoil), data=varechem)
 vare.cca
